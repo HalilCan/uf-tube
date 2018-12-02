66e9e7141da461dca43a6391004c4caf15633204
HalilCan <halilcanmemoglu@gmail.com>
2018-12-02 10:16:04 -0500

    setup

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-hello php.
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>unfuckTube</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="/stylesheets/main.css" />
+    <script src="scripts/main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
