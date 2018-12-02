4304c549416e670c5018fd7652b612bfc56a61ee
HalilCan <halilcanmemoglu@gmail.com>
2018-12-02 10:25:12 -0500

    stlying

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>unfuckTube</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="/stylesheets/main.css" />
-    <script src="scripts/main.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/stylesheets/main.css" />
+    <script src="assets/scripts/main.js"></script>
 </head>
 <body>
     <div id="pageContainer">
         <div id="mastHeadContainer">
-        
+        Hey
         </div>
         <div id="sideNavContainer">
-            
-        </div>        
+
+        </div>
+        <div id="mainSectionContainer">
+        
+        </div>
     </div>
     
 </body>
