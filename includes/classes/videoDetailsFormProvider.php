61c2e4a7e0e1e57f4d6f5f5391c3b546e507eb0b
HalilCan <halilcanmemoglu@gmail.com>
2018-12-05 13:35:28 -0500

    oh so this what php does

---
+++ includes/classes/videoDetailsFormProvider.php
@@ -0,0 +1,8 @@
+<?php 
+    class VideoDetailsFormProvider {
+        public function createUploadForm() {
+            return "<form action='processing.php' method='POST'>
+                    </form>";
+        }
+}
+?>
--- upload.php
+++ upload.php
@@ -1,5 +1,13 @@
-<?php require_once("includes/header.php");?>
+<?php 
+require_once("includes/header.php");
+require_once("includes/classes/VideoDetailsFormProvider.php");
+?>
 
-
+<div class="column">
+    <?php  
+        $formProvider = new VideoDetailsFormProvider();
+        echo $formProvider -> createUploadForm();
+    ?>
+</div>
 
 <?php require_once("includes/footer.php");?>
