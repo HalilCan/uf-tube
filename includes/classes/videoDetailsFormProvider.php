68beee60b44ba41336c89c4f91b62b4745583c34
HalilCan <halilcanmemoglu@gmail.com>
2018-12-05 13:56:23 -0500

    upload form fleshed out

--- includes/classes/videoDetailsFormProvider.php
+++ includes/classes/videoDetailsFormProvider.php
@@ -1,8 +1,47 @@
 <?php 
     class VideoDetailsFormProvider {
         public function createUploadForm() {
+            $fileInput = $this -> createFileInput();
+            $titleInput = $this -> createTitleInput();
+            $descriptionInput = $this -> createDescriptionInput();
+            $privacyInput = $this -> createPrivacyInput();
+
             return "<form action='processing.php' method='POST'>
+                        $fileInput
+                        $titleInput
+                        $descriptionInput
+                        $privacyInput
                     </form>";
+        }
+
+        private function createFileInput() {
+            /* If you use a label, it must match the input id: <label for='exampleFormControlFile1'>Your file</label> */
+            return "<div class='form-group'>
+                <input type='file' class='form-control-file' name='fileInput' required>
+            </div>";
+        }
+
+        private function createTitleInput() {
+            return "<div class='form-group'>
+            <input class='form-control' type='text' placeholder='Title' name='titleInput'>
+            </div>";
+        }
+
+        
+        private function createDescriptionInput() {
+            return "<div class='form-group'>
+            <textarea class='form-control' placeholder='Description' name='descriptionInput' rows='3'></textarea>
+            </div>";
+        }
+
+        private function createPrivacyInput() {
+            return "<div class='form-group'>
+            <select class='form-control' name='privacyInput'>
+              <option value='0'>Private</option>
+              <option value='1'>Public</option>
+            </select>
+          </div>";
+            
         }
 }
 ?>
