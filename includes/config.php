b243caca8fc0ce806b70acc2ed14f09f1814fea0
HalilCan <halilcanmemoglu@gmail.com>
2018-12-05 14:09:19 -0500

    pdo for higher security

--- includes/config.php
+++ includes/config.php
@@ -2,4 +2,12 @@
     ob_start(); //turns on output buffering
 
     date_default_timezone_set("America/New_York");
+
+    try {
+        $con = new PDO("mysql:dbname=uftube;host=localhost", "root", ""); //config, admin login, pw
+        $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); //sets the first attribute to the second value
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
 ?>
