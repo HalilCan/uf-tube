3dd34e3407af8972d72d3c5c2eb004bfbbca8908
HalilCan <halilcanmemoglu@gmail.com>
2018-12-08 18:02:52 -0500

    fixing the no file bug

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -17,17 +17,28 @@ class VideoProcessor {
         $tempFilePath = $targetDir . uniqid() . basename($videoData["name"]); //uniqid is a core php function, it's automatically generated
         $tempFilePath = str_replace(" ", "_", $tempFilePath);
 
-        $isValudData = $this->processData($videoData, $tempFilePath);
+        $isValidData = $this->processData($videoData, $tempFilePath);
+        
+        if (!$isValidData) {
+            return false;
+        }
+
+        if (move_uploaded_file($videoData["tmp_name"], $tempFilePath)) { //if the file path is valid, the file will be moved
+            echo "File uploaded successfully.";
+        }
     }
 
     private function processData($videoData, $filePath) {
         $videoType = pathInfo($filePath, PATHINFO_EXTENSION);
 
-        if (!this->isValidSize($videoData)) {
+        if (!$this->isValidSize($videoData)) {
             echo("File too large, can't be larger than " . $this->sizeLimit . " bytes.");
             return false;
-        } else if (!this->isValidTyoe($videoType)) {
+        } else if (!$this->isValidType($videoType)) {
             echo("Invalid file type.");
+            return false;
+        } else if ($this->hasError($videoData)) {
+            echo("Error code: " . $videoData["error"]);
             return false;
         }
     }
@@ -39,6 +50,10 @@ class VideoProcessor {
     private function isValidType($type) {
         $lowercased = strtolower($type);
         return in_array($lowercased, $this->allowedTypes);
+    }
+    
+    private function hasError($data) {
+        return $data["error"] != 0;
     }
 }
 ?>
