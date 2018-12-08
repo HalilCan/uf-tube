632627f7d2f9ed4caad5a7713eb8e396ef541182
HalilCan <halilcanmemoglu@gmail.com>
2018-12-07 23:58:06 -0500

    upload function

--- includes/classes/VideoUploadData.php
+++ includes/classes/VideoUploadData.php
@@ -8,6 +8,8 @@ class VideoUploadData {
     }
 
     public function upload($videoUploadData) {
+
+        $targetDir = "uploads/videos/"; //Videos won't be in a table, the paths to the videos will be.
         
     }
 }
