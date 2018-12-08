848759d0fb2fc4e56bfe45261c8333301d2a653d
HalilCan <halilcanmemoglu@gmail.com>
2018-12-07 23:46:24 -0500

    videoUploadData

---
+++ includes/classes/VideoUploadData.php
@@ -0,0 +1,8 @@
+<?php 
+class VideoUploadData {
+    public function __construct() {
+        
+    }
+}
+
+?>
--- processing.php
+++ processing.php
@@ -1,7 +1,19 @@
 <?php 
 require_once("includes/header.php");
+
+if(!isSet($_POST["uploadButton"])) { //we 'retrieve' the POSTed data through $_POST, apparently. We'll see.
+    echo "No file found for upload!";
+    exit();
+}
+
+// 1- Create file upload data
+
+
+// 2- Process video data
+
+
+// 3- Check if upload was successful 
+
+
 ?>
-
-
-
 <?php require_once("includes/footer.php");?>
