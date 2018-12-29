e7089c60ca998500a48e7f3b0ef02139bb606785
HalilCan <halilcanmemoglu@gmail.com>
2018-12-29 07:31:19 -0500

    expanded user class

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -5,6 +5,32 @@ class User {
 
     public function __construct($con, $username) {
         $this->con = $con;
+
+        $query = $this->con->prepare("SELECT * FROM users WHERE username=:un");
+        $query->bindParam(":un", $username);
+        $query->execute();
+
+        $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getUsername() {
+        return $this->sqlData["username"];
+    }
+ 
+    public function getName() {
+        return $this->sqlData["firstName"] . " " . $this->sqlData["lastName"];
+    }
+
+    public function getFirstName() {
+        return $this->sqlData["firstName"];
+    }
+    
+    public function getLastName() {
+        return $this->sqlData["lastName"];
+    }
+    
+    public function getEmail() {
+        return $this->sqlData["email"];
     }
 }
 
