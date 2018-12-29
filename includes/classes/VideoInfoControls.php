7709e6263dcd3d1b88e89af6c46121ca256fcfef
HalilCan <halilcanmemoglu@gmail.com>
2018-12-29 15:26:33 -0500

    video likes, database structure deviation
    Former-commit-id: 18a45a457d7762abe4981eae019e58745fdb9317

---
+++ includes/classes/VideoInfoControls.php
@@ -0,0 +1,42 @@
+<?php 
+
+require_once("includes/classes/ButtonProvider.php");
+
+class VideoInfoControls {
+    private $video, $userLoggedInObj;
+
+    public function __construct($video, $userLoggedInObj) {
+        $this->video = $video;
+        $this->userLoggedInObj = $userLoggedInObj;
+    }
+
+    public function create() {
+        $likeButton = $this->createLikeButton();
+        $dislikeButton = $this->createDislikeButton();
+
+
+        return "<div class='controls'>
+                    $likeButton
+                    $dislikeButton
+                </div>";
+    }
+
+    private function createLikeButton() {
+        $text = $this->video->getLikes();
+        $videoId = $this->video->getId();
+        $action = "likeVideo(this, $videoId)";
+        $class = "likeButton";
+
+        $imageSrc = "assets/images/icons/thumb-up.png";
+
+        //todo: change button if video has been liked already, same for dislike
+
+        return ButtonProvider::createButton("Like", $imageSrc, $action, $class);
+    }
+    
+    private function createDislikeButton() {
+        return ButtonProvider::createButton("Dislike", "", "", ""); 
+    }
+}
+
+?>
--- includes/classes/VideoInfoSection.php
+++ includes/classes/VideoInfoSection.php
@@ -1,5 +1,7 @@
 <?php 
 
+require_once("includes/classes/VideoInfoControls.php");
+
 class VideoInfoSection {
     private $con, $video, $userLoggedInObj;
 
@@ -17,10 +19,14 @@ class VideoInfoSection {
         $title = $this->video->getTitle();
         $views = $this->video->getViews();
 
+        $videoInfoControls = new VideoInfoControls($this->video, $this->userLoggedInObj);
+        $controls = $videoInfoControls->create();
+
         return "<div class='videoInfo'
                     <h1>$title</h1>
                     <div class='bottomSection'>
                         <span class='viewCount'>$views</span>
+                        $controls
                     </div>
                 </div>";
     }
