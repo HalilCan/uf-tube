fac149eaff446a60c1c656cc92b3a5828ebd22f4
HalilCan <halilcanmemoglu@gmail.com>
2018-12-30 07:44:46 -0500

    l/d styling
    Former-commit-id: e479dc9da8acd5d2cdbd25ca36d25cb5f20f93e0

--- assets/scripts/videoPlayerActions.js
+++ assets/scripts/videoPlayerActions.js
@@ -5,5 +5,21 @@ function likeVideo(button, videoId) {
 
         let likeButton = $(button);
         let dislikeButton = $(button).siblings(".dislikeButton");
+
+        likeButton.addClass("active");
+        dislikeButton.removeClass("active");
+    });
+}
+
+function dislikeVideo(button, videoId) {
+    $.post("ajax/likeVideo.php", {videoId: videoId})
+    .done(function(data){
+        alert(data);
+
+        let dislikeButton = $(button);
+        let likeButton = $(button).siblings(".likeButton");
+
+        dislikeButton.addClass("active");
+        likeButton.removeClass("active");
     });
 }
