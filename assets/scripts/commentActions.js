87e1c46d7c4b8ec54dc7a7590818f6d52bf191e7
HalilCan <halilcanmemoglu@gmail.com>
2018-12-31 14:52:57 -0500

    .parent().parent() depth difference worked
    Former-commit-id: 7b2c71c93daa0e95ff1c47a8c02158351adee690

--- assets/scripts/commentActions.js
+++ assets/scripts/commentActions.js
@@ -6,7 +6,12 @@ function postComment(button, postedBy, videoId, replyTo, containerClass) {
     if (commentText) {
         $.post("ajax/postComment.php", {commentText: commentText, postedBy: postedBy, videoId: videoId, responseTo: replyTo})
         .done(function(comment){
-            $("." + containerClass).prepend(comment);
+
+            if(!replyTo) {
+                $("." + containerClass).prepend(comment);
+            } else {
+                $(button).parent().parent().siblings("." + containerClass).append(comment); 
+            }
         });
 
     } else {
