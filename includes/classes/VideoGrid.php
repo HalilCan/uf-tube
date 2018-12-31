e518ed335cd52a60d6a230d0d159fefe6adabbba
HalilCan <halilcanmemoglu@gmail.com>
2018-12-31 16:25:17 -0500

    generateGrid
    Former-commit-id: 2dd66f77dc86cd1b5975ac4d47133b11e0ef7f2e

--- includes/classes/VideoGrid.php
+++ includes/classes/VideoGrid.php
@@ -12,11 +12,48 @@ class VideoGrid {
     }
 
     public function create($videos, $title, $showFilter) {
-        return "<div class='$this->gridClass'> 
-        
+
+        if ($videos == null) {
+            $gridItems = $this->generateItems();
+        } else {
+            $gridItems = $this->generateItemsFromVideos($videos);
+        }
+
+        $header = "";
+
+        if ($title != null) {
+            $header = $this->createGridHeader($title, $showFilter);
+        }
+
+        return "
+        $header
+        <div class='$this->gridClass'> 
+
         </div>";
     }
 
+    public function generateItems() {
+        $query = $this->con->prepare("SELECT * FROM videos ORDER BY RAND() LIMIT 15");
+        $query->execute();
+
+        $elementsHtml = "";
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $video = new Video($this->con, $this->userLoggedInObj);
+            $item = new VideoGridItem($video, $this->largeMode);
+
+            $elementsHtml .= $item->create();
+        }
+
+        return $elementsHtml;
+    }
+
+    public function generateItemsFromVideos($videos) {
+
+    }
+
+    public function createGridHeader($title, $showFilter) {
+        return "";
+    }
 }
 
 ?>
