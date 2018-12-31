215521290abb69ca1f29ad7c612e8fc487fc581e
HalilCan <halilcanmemoglu@gmail.com>
2018-12-31 13:12:07 -0500

    like/dislike comments
    Former-commit-id: 2e8a2bff894dfcfc2ad99988199c3c8ee5d7f717

--- assets/scripts/commentActions.js
+++ assets/scripts/commentActions.js
@@ -20,3 +20,51 @@ function toggleReply(button) {
 
     commentForm.toggleClass("hidden");
 }
+
+function likeComment(commentId, button, videoId) {
+    $.post("ajax/likeComment.php", {commentId: commentId})
+    .done(function(data){
+        
+        let likeButton = $(button);
+        let dislikeButton = $(button).siblings(".dislikeButton");
+
+        likeButton.addClass("active");
+        dislikeButton.removeClass("active");
+
+        let result = JSON.parse(data);
+        updateLikesValue(likeButton.find(".text"), result.likes);
+        updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+
+        if(result.likes < 0) {
+            likeButton.removeClass("active");
+            likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+        } else {
+            likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up-active.png");
+        }
+        dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+    });
+}
+
+function dislikeComment(commentId, button, videoId) {
+    $.post("ajax/dislikeComment.php", {commentId: commentId})
+    .done(function(data){
+
+        let dislikeButton = $(button);
+        let likeButton = $(button).siblings(".likeButton");
+
+        dislikeButton.addClass("active");
+        likeButton.removeClass("active");
+
+        let result = JSON.parse(data);
+        updateLikesValue(likeButton.find(".text"), result.likes);
+        updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+
+        if(result.dislikes < 0) {
+            dislikeButton.removeClass("active");
+            dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+        } else {
+            dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down-active.png");
+        }
+        likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+    });
+}
