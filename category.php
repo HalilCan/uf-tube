124ce1e13db9f9513af89da2ade42478b96567b8
HalilCan <halilcanmemoglu@gmail.com>
2019-12-25 05:06:07 -0500

    category page done

--- category.php
+++ category.php
@@ -1,9 +1,8 @@
 <?php
     require_once("includes/header.php");
-    require_once("includes/classes/VideoGrid.php");
-    $category = "";
+    require_once("includes/classes/VideoGridItem.php");
     $catId;
-    $vidList;
+    $vidList = "";
 
     if(!isset($_GET["category"])) {
             // redirect to homepage if no cat available
@@ -11,23 +10,23 @@
     } else {
         $catId = $_GET["category"];
     }
-
-    $query = $this->con->prepare("SELECT * FROM categories WHERE id=:id");
-    $query->bindParam(":id", $catId);
-    $query->execute();
-
-    $catEntry = $query->fetch(PDO::FETCH_ASSOC);
-    $category = $catEntry["name"];
     
-    $query = $this->con->prepare("SELECT * FROM videos WHERE category=:cat");
-    $query->bindParam(":cat", $category);
+    $query = $con->prepare("SELECT * FROM videos WHERE category=:cat");
+    $query->bindParam(":cat", $catId);
     $query->execute();
     
     while($videoEntry = $query->fetch(PDO::FETCH_ASSOC)) {
-        //gen and add video 
+        //gen and add video box
+        $videoItem = new Video($con, $videoEntry, $userLoggedInObj);
+        $videoGridItem = new VideoGridItem($videoItem, 0, 50);
+        $videoElement = $videoGridItem->create();
+
+        $vidList .= "<div class='videoGridItem'>
+                        $videoElement
+                    </div>";
     }
 
-    echo "WELL HELLO, " . $username;
+    echo "<div class='categoryVideoList'>$vidList</div>";
 ?>
 
 <?php require_once("includes/footer.php");?>
